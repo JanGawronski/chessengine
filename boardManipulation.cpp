59463eda8b4c55227402f806f53254692e91d341
JanGawronski <jangawronski04@gmail.com>
2025-02-23 18:01:13 +0100

    Improve move making logic

--- boardManipulation.cpp
+++ boardManipulation.cpp
@@ -1,16 +1,24 @@
 #include "Board.h"
 #include "Move.h"
+#define WHITE 0
+#define BLACK 1
 
 Board makeMove(Board board, Move move) {
+    int playerIndex = 0 ? board.sideToMove == WHITE : 6;
+    int opponentIndex = 6 ? board.sideToMove == BLACK : 0;
     Board newBoard = board;
 
     // Update bitboards
-    newBoard.bitboards[move.piece] &= ~(1ULL << move.from);
-    newBoard.bitboards[move.piece] |= 1ULL << move.to;
+    newBoard.bitboards[move.piece + playerIndex] &= ~(1ULL << move.from);
+    newBoard.bitboards[move.piece + playerIndex] |= 1ULL << move.to;
 
     if (move.capture) {
-        int capturePiece = move.capture + 5 * (1 - board.sideToMove);
-        newBoard.bitboards[capturePiece] &= ~(1ULL << move.to);
+        newBoard.bitboards[move.capture - 1 + opponentIndex] &= ~(1ULL << move.to);
+    }
+
+    if (move.promotion) {
+        newBoard.bitboards[move.promotion + playerIndex] |= 1ULL << move.to;
+        newBoard.bitboards[move.piece + playerIndex] &= ~(1ULL << move.to);
     }
 
     // Update side to move
