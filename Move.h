f08911c48672bfc9caaad89299a9f69f7a293ed3
JanGawronski <jangawronski04@gmail.com>
2025-02-23 22:39:38 +0100

    Added string representation of move

--- Move.h
+++ Move.h
@@ -1,6 +1,8 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+#include <string>
+
 struct Move {
     int from; // 0-63
     int to; // 0-63
@@ -10,5 +12,25 @@ struct Move {
     int enPassant; // 0: none, 1: white, 2: black
     int castling; // 0: none, 1: white king side, 2: white queen side, 3: black king side, 4: black queen side
 };
+
+inline std::string getMoveString(Move move)
+{
+    std::string str = "";
+    str += "NBRQK"[move.piece - 1];
+    str += (char)('a' + move.from % 8);
+    str += (char)('1' + move.from / 8);
+    if (move.capture)
+    {
+        str += "x" + "NBRQK"[move.capture - 2];
+    }
+
+    str += (char)('a' + move.to % 8);
+    str += (char)('1' + move.to / 8);
+    if (move.promotion)
+    {
+        str += "=" + "NBRQ"[move.promotion];
+    }
+    return str;
+}
 
 #endif // MOVE_H
